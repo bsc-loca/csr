631b0b26a8c5fbd0bc981c67c9de023171ae54c5
Arnau Bigas <arnau.bigas@bsc.es>
2023-11-03 12:08:43 +0100

    Add LICENSE, CONTRIBUTORS, update headers

---
+++ CONTRIBUTORS.md
@@ -0,0 +1,6 @@
+- Arnau Bigas Soldevilla (arnau.bigas@bsc.es)
+- Carlos Rojas (carlos.rojas@bsc.es)
+- Gerard Candón (gerard.candon@bsc.es)
+- Max Doblas-Font (max.doblas@bsc.es)
+- Narcís Rodas (narcis.rodaquiroga@bsc.es)
+- Xavier Carril (xavier.carril@bsc.es)
--- rtl/csr_bsc.sv
+++ rtl/csr_bsc.sv
@@ -1,22 +1,22 @@
-//////////////////////////////////////////////////////////////////////////////////
-// Company: Barcelona Supercomputing Center
-// Engineer: Max Doblas Font max.doblas@bsc.es
-// 
-// Create Date: 29/08/2019 17:06:00 PM
-// Design Name: 
-// Module Name: csr_bsc
-// Project Name: DRAC
-// Target Devices: 
-// Tool Versions: Privileged ISA 1.11
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
+/*
+ * Copyright 2023 BSC*
+ * *Barcelona Supercomputing Center (BSC)
+ * 
+ * SPDX-License-Identifier: Apache-2.0 WITH SHL-2.1
+ * 
+ * Licensed under the Solderpad Hardware License v 2.1 (the “License”); you
+ * may not use this file except in compliance with the License, or, at your
+ * option, the Apache License version 2.0. You may obtain a copy of the
+ * License at
+ * 
+ * https://solderpad.org/licenses/SHL-2.1/
+ * 
+ * Unless required by applicable law or agreed to in writing, any work
+ * distributed under the License is distributed on an “AS IS” BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
 
 module csr_bsc#(
     parameter word_width = 64,
